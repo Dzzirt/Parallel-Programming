c8df207738d2e90dabf6afe585a9d1a00ea9e15c
Nikita Kuzin <nikita.kuzin@my-mac.ispring.lan>
2016-09-28 22:04:41 +0300

    добавил поиск определителя и миноров

--- lab1-matrix/src/Main.java
+++ lab1-matrix/src/Main.java
@@ -1,17 +1,27 @@
+import java.util.Random;
+
 /**
  * Created by Nikita on 21.09.2016.
  */
 public class Main {
 
     public static void main(String[] args) {
-        float[][] mat = new float[][]{
-                {1, 2, 3, 4},
-                {5, 6, 7, 8},
-                {9, 0, 1, 2},
-                {3, 4, 5, 6}
-        };
+        Random random = new Random();
+        float[][] mat = new float[3][3];
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                mat[i][j] = random.nextInt(5);
+            }
+
+        }
         try {
-            final float determinant = MathUtils.getDeterminant(mat);
+            long startTime = System.currentTimeMillis();
+            final float[][] minors = MathUtils.getMinorMatrix(mat);
+
+            long endTime   = System.currentTimeMillis();
+            long totalTime = endTime - startTime;
+
+            System.out.println(totalTime);
         } catch (MathUtils.MatrixIsNotSqaure matrixIsNotSqaure) {
             matrixIsNotSqaure.printStackTrace();
         }
--- lab1-matrix/src/MathUtils.java
+++ lab1-matrix/src/MathUtils.java
@@ -12,29 +12,56 @@ public class MathUtils {
         return new float[2][2];
     }
 
+    public static float[][] getMinorMatrix(float[][] inputMat) throws MatrixIsNotSqaure {
+        float[][] outputMat = new float[inputMat.length][inputMat.length];
+        for (int row = 0; row < inputMat.length; row++) {
+            for (int col = 0; col < inputMat[0].length; col++) {
+                outputMat[row][col] = getDeterminant(decreaseSize(inputMat, row, col));
+            }
+        }
+        return outputMat;
+    }
     public static float getDeterminant(float[][] inputMat) throws MatrixIsNotSqaure {
         int colNum = inputMat[0].length;
         int rowNum = inputMat.length;
         if (colNum != rowNum) {
             throw new MatrixIsNotSqaure();
         }
+        if (inputMat.length == 2) {
+            return inputMat[0][0] * inputMat[1][1] - inputMat[0][1] * inputMat[1][0];
+        }
         float[][] charMat = getCharactersMatrix(colNum);
         float determinant = 0;
         for (int col = 0; col < inputMat.length; col++) {
-            float[][] subMat =
-            //determinant += inputMat[0][col] * getDeterminant()
+            float[][] subMat = decreaseSize(inputMat, 0, col);
+            determinant += inputMat[0][col] * getDeterminant(subMat) * charMat[0][col];
         }
-        System.out.println(1);
-        return 1;
+        return determinant;
     }
 
     private static float[][] decreaseSize(float[][] inputMat, int rmRow, int rmCol) {
+        int newSize = inputMat.length - 1;
+        int rowCarret = 0;
+        int colCarret = 0;
+        boolean moveRowCarret = false;
+        float[][] outputMat = new float[newSize][newSize];
         for (int i = 0; i < inputMat.length; i++) {
             for (int j = 0; j < inputMat[0].length; j++) {
-
+                if (i != rmRow && j != rmCol) {
+                    outputMat[rowCarret][colCarret] = inputMat[i][j];
+                    colCarret++;
+                    moveRowCarret = true;
+                }
+            }
+            colCarret = 0;
+            if (moveRowCarret) {
+                moveRowCarret = false;
+                rowCarret++;
             }
         }
+        return outputMat;
     }
+
     private static float[][] getCharactersMatrix(int size) {
         final float[][] mat = new float[size][size];
         for (int i = 0; i < mat.length; i++) {
